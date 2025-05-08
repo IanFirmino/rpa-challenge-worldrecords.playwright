95e102b2b22142fa626d3e44355daf5ddb248d49
IanFirmino <ian.firmino@hotmail.com>
2025-05-08 16:35:06 -0300

    feat: implementacao da captura de todos os records, records por titulo e records por categoria

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# rpa-challenge-wordrecords.webscraping
-Repositório do desafio para a vaga de RPA da BTime.
+# WebScraping do site do Livro dos recordes!
+
+Essa captura de dados foi realiza com o framework **Playwright** seguindo os princípios da arquitetura limpa com exportação dos dados para CSV e logs de erros.
+
+## Arquitetura
+|                 |                           						|
+|-----------------|-------------------------------------------------|
+|/main			  |`Arquivo inicial`              				   	|
+|/model           |`Entidades do sistema`          				   	|
+|/controller      |`Caso de uso do sistema`            			   	|
+|/service         |`Funções de auxilio para o controlador`          |
+|/utils           |`Utilitários e funções de auxilio para o serviço`|
+
+
+## Instalação
+
+- Criar o ambiente virtual `python -m venv venv`
+- Instalar as dependências `pip install -r requirements.txt`
+
+## Execução
+
+- ``` python main.py get_all ```
+Execute a captura de todos os recordes.
+
+- ``` python main.py get_by_category --category animais ```
+Execute a captura dos recordes que tem a categoria "animais".
+
+- ``` python main.py get_by_title --title veneno ```
+Execute a captura dos recordes que tem o titulo "veneno".
--- src/main.py
+++ src/main.py
@@ -1,7 +1,29 @@
+import argparse
 from src.controller.controller_records import get_all_records, get_records_by_title, get_records_by_category
 
 def main():
-   get_all_records()
+    parser = argparse.ArgumentParser(description="Script para manipulação de registros")
+    subparsers = parser.add_subparsers(dest="command", help="Subcomandos disponíveis")
+
+    # parametro --get_all
+    subparser_get_all = subparsers.add_parser("get_all", help="Obter todos os registros")
+    subparser_get_all.set_defaults(func=get_all_records)
+
+    # parametro --get_by_category
+    subparser_get_by_category = subparsers.add_parser("get_by_category", help="Obter registros por categoria")
+    subparser_get_by_category.add_argument("--category", required=True, help="Categoria dos registros")
+    subparser_get_by_category.set_defaults(func=get_records_by_category)
+
+    # parametro --get_by_title
+    subparser_get_by_title = subparsers.add_parser("get_by_title", help="Obter registros por título")
+    subparser_get_by_title.add_argument("--title", required=True, help="Título dos registros")
+    subparser_get_by_title.set_defaults(func=get_records_by_title)
+
+    args = parser.parse_args()
+    if hasattr(args, "func"):
+        args.func(args)
+    else:
+        parser.print_help()
 
 if __name__ == '__main__':
     main()
